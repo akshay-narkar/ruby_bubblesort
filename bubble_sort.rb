387593d06e6c29add71394e02ffd704df694e7b5
akshay-narkar <akshaynarkar1295@gmail.com>
2020-11-18 15:16:52 +0530

    Bubble sort for string

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -8,4 +8,20 @@ def bubble_sort(arr)
   end
 end
 
+def bubble_sort_by(arr)
+  arr.each_with_index do |_value, _index|
+    arr.each_with_index do |_value, index|
+      next unless index < arr.length - 1
+
+      arr[index], arr[index + 1] = arr[index + 1], arr[index] if yield(arr[index], arr[index + 1]).positive?
+    end
+  end
+end
+
+result = bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
+
+p result
+
 p bubble_sort([4, 3, 55, 2, 0, 2])
